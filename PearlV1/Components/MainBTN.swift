1e160c686b0e0cd8ce6f713a775d413ec7c036ba
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-09 00:33:50 +0200

    Alright alright , my first reusable button is here

---
+++ PearlV1/Components/MainBTN.swift
@@ -0,0 +1,18 @@
+//
+//  MainBTN.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 09/12/2023.
+//
+
+import SwiftUI
+
+struct MainBTN: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainBTN()
+}

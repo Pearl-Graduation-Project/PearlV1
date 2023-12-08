09fc052144b9a76b8b333a95b41cdb63bf73b8e0
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-09 00:21:32 +0200

    OTP added

--- PearlV1/Components/OTPTextField.swift
+++ PearlV1/Components/OTPTextField.swift
@@ -1,18 +1,65 @@
 //
 //  OTPTextField.swift
-//  PearlV1
+//  Pickup
 //
-//  Created by ElAmir Mansour on 09/12/2023.
+//  Created by ElAmir Mansour 2023-10-15.
 //
 
 import SwiftUI
 
 struct OTPTextField: View {
+    let numberOfFields: Int
+    
+    @Binding var enterValue: [String]
+    @FocusState private var fieldFoucus: Int?
+    @State private var oldValue: String = ""
+    
+    init(numberOfFields: Int, enterValue: Binding<[String]>) {
+        self.numberOfFields = numberOfFields
+        self._enterValue = enterValue
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(0..<numberOfFields, id: \.self) { index in
+                TextField("", text: $enterValue[index], onEditingChanged: { editing in
+                    if editing {
+                        oldValue = enterValue[index]
+                    }
+                })
+                    .keyboardType(.numberPad)
+                    .frame(width: 48, height: 48)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+                    .multilineTextAlignment(.center)
+                    .focused($fieldFoucus, equals: index)
+                    .tag(index)
+                    .onChange(of: enterValue[index]) { _, newValue in
+                        if enterValue[index].count > 1 {
+                            let currentValue = Array(enterValue[index])
+                            if currentValue[0] == Character(oldValue) {
+                                enterValue[index] = String(enterValue[index].suffix(1))
+                            } else {
+                                enterValue[index] = String(enterValue[index].prefix(1))
+                            }
+                        }
+                        if !newValue.isEmpty {
+                            if index == numberOfFields - 1 {
+                                fieldFoucus = nil
+                            } else {
+                                fieldFoucus = (fieldFoucus ?? 0) + 1
+                            }
+                        } else {
+                            fieldFoucus = (fieldFoucus ?? 0) - 1
+                        }
+                    }
+
+
+            }
+        }
     }
 }
 
 #Preview {
-    OTPTextField()
+     OTPTextField(numberOfFields: 6, enterValue: .constant(["", "", "", "", "", ""]))
 }

703dfba918609fedf26f0ec071161a4ea3bad8c7
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-08 18:04:21 +0200

    Onboarding model modified

--- PearlV1/Models/OnBoardingModel.swift
+++ PearlV1/Models/OnBoardingModel.swift
@@ -1,8 +1,25 @@
 //
-//  OnBoardingModel.swift
-//  PearlV1
+//  onBoardingModel.swift
+//  Pearl
 //
-//  Created by ElAmir Mansour on 08/12/2023.
+//  Created by ElAmir Mansour on 07/12/2023.
 //
 
 import Foundation
+
+struct Page: Identifiable ,Equatable{
+    let id = UUID()
+    var name: String
+    var description: String
+    var imageUrl: String
+    var tag: Int
+    
+    static var samplePage = Page(name: "TITLE", description: "This is a description", imageUrl: "First_Pic", tag: 0)
+    
+    static var samplePages: [Page] = [
+        Page(name: "Analise your skin and know its type", description: "Lorem ipsum dolor sit amet consectetur. Vestibulum nam sed platea bibendum libero tempor faucibus.", imageUrl: "First_Pic", tag: 0),
+        Page(name: "Get your morning skin routine", description: "Lorem ipsum dolor sit amet consectetur. Vestibulum nam sed platea bibendum libero tempor faucibus.", imageUrl: "Second_Pic", tag: 1),
+        Page(name: "And your night skin routine", description: "Lorem ipsum dolor sit amet consectetur. Vestibulum nam sed platea bibendum libero tempor faucibus.", imageUrl: "Third_Pic", tag: 2)
+
+    ]
+}

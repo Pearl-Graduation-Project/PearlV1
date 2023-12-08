5d9beda430e5dcc3adb0d02980309b61cc284ad2
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-08 18:07:23 +0200

    assets and onboarding mvvm

---
+++ PearlV1/Views/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 08/12/2023.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}

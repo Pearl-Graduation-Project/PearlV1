93684d021b54ec2e97c9e2bd2c44fe1480633da3
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-09 21:08:55 +0200

    Fixing some bugs

---
+++ PearlV1/Views/InitialvIEW.swift
@@ -0,0 +1,18 @@
+//
+//  InitialvIEW.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 09/12/2023.
+//
+
+import SwiftUI
+
+struct InitialvIEW: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialvIEW()
+}

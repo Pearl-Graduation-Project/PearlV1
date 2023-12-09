a4b277c8e532ed5dad30104328c757cd10e2f0d2
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-09 23:11:38 +0200

    Added gradient for first page

--- PearlV1/Views/InitialView.swift
+++ PearlV1/Views/InitialView.swift
@@ -34,24 +34,43 @@ struct InitialView: View {
 }
 
 
-
-
 struct LogoView: View {
     var body: some View {
-        Image("Logo")
-            .foregroundColor(.white)
-            .font(.largeTitle)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color(hex: 0xD6E4FF), location: 0.0),
+                    .init(color: Color(hex: 0xE3FFE3), location: 0.3),
+                    .init(color: Color(hex: 0xFFD4DE), location: 0.7),
+                    .init(color: Color(hex: 0xFFF8F3), location: 1.0),
+                ]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            .edgesIgnoringSafeArea(.all)
+            .blur(radius: 20)
+            Image("Logo")
+                .resizable()
+                .frame(width: 92, height: 174)
+                .aspectRatio(contentMode: .fit)
+                .scaledToFit()
+                .padding()
+                .cornerRadius(30)
+                .frame(width: 200)
+        }
     }
 }
+
 
 #if DEBUG
 #Preview{
         InitialView()
     }
 #endif
-extension CGSize: Equatable {
+extension CGSize {
     public static func == (lhs: CGSize, rhs: CGSize) -> Bool {
         return lhs.width == rhs.width && lhs.height == rhs.height
     }
 }
+
 

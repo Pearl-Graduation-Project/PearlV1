39951b03ffbb6d9f1458469a7aaa9b7ef4accf0b
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-09 12:44:59 +0200

    Loader view

--- PearlV1/Components/LoaderView.swift
+++ PearlV1/Components/LoaderView.swift
@@ -1,18 +1,44 @@
 //
 //  LoaderView.swift
-//  PearlV1
+//  Pickup
 //
-//  Created by ElAmir Mansour on 09/12/2023.
+//  Created by ElAmir on 2023-10-13.
 //
 
 import SwiftUI
 
 struct LoaderView: View {
+    @State private var showSpinner:Bool = false
+    @State private var degree:Int = 270
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(.blackC)
+            .overlay {
+                Circle()
+                    .trim(from: 0.0, to: 0.6)
+                    .stroke(
+                        style:
+                            StrokeStyle(
+                                lineWidth: 2,
+                                lineCap: .round,
+                                lineJoin: .round
+                            )
+                    )
+                    .animation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: showSpinner)
+                    .rotationEffect(Angle(degrees: Double(degree)))
+                    .animation(Animation.linear(duration: 1.5).repeatForever(autoreverses: false), value: showSpinner)
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.white)
+                    .onAppear {
+                        showSpinner = true
+                        degree = 270 + 360
+                    }
+            }
+            .frame(width: 48)
     }
 }
 
 #Preview {
-    LoaderView()
-}
+        LoaderView()
+    }

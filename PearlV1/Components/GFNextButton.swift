1be9ce6e7034527db8e49eb6442931096750bf64
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-10 19:58:37 +0200

    Buttons

---
+++ PearlV1/Components/GFNextButton.swift
@@ -0,0 +1,18 @@
+//
+//  GFNextButton.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 10/12/2023.
+//
+
+import SwiftUI
+
+struct GFNextButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GFNextButton()
+}
---
+++ PearlV1/ViewModels/LoginViewModel/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 10/12/2023.
+//
+
+import SwiftUI
+
+struct LoginViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginViewModel()
+}

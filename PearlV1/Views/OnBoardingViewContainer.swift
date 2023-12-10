f257c73ff668abe39f357943b5e990d6197e7c8b
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-10 22:24:16 +0200

    Onboarding fixed

---
+++ PearlV1/Views/OnBoardingViewContainer.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 10/12/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}

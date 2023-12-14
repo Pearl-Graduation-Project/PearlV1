be57046e5f5ba8c989f3a3d7b7fe409ed2ab26d1
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-15 01:11:45 +0200

    Navbar custom

---
+++ PearlV1/Components/Navbar.swift
@@ -0,0 +1,18 @@
+//
+//  Navbar.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 15/12/2023.
+//
+
+import SwiftUI
+
+struct Navbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navbar()
+}

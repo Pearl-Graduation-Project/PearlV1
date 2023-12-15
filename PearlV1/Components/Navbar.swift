2e73214be90b5d627cb32aadc8ec3ad60761abe4
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-12-16 00:39:54 +0200

    refactored navbar

--- PearlV1/Components/Navbar.swift
+++ PearlV1/Components/Navbar.swift
@@ -4,80 +4,50 @@
 //
 //  Created by ElAmir Mansour on 15/12/2023.
 //
-
 import SwiftUI
+
+struct NavigationElement: Identifiable {
+    let id = UUID()
+    let title: String
+    let imageName: String
+    let destination: Text
+}
+
 struct Navbar: View {
+    let navigationElements: [NavigationElement] = [
+        NavigationElement(title: "Home", imageName: "HomeNotClicked", destination: Text("Element 1 Details")),
+        NavigationElement(title: "Progress", imageName: "ProgressSF", destination: Text("Element 2 Details")),
+        NavigationElement(title: "Products", imageName: "ProdcutsIconSF", destination: Text("Element 3 Details")),
+        NavigationElement(title: "Routine", imageName: "RoutineIconSF", destination: Text("Element 4 Details")),
+        NavigationElement(title: "Community", imageName: "CommunitySF", destination: Text("Element 5 Details")),
+    ]
+
     var body: some View {
         NavigationView {
             VStack {
-                // Your custom navigation bar with 5 elements
                 HStack {
-                    NavigationLink(destination: Text("Element 1 Details")) {
-                        VStack {
-                            Image("HomeNotClicked") // Replace with your image
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                            Text("Home").customTextStyle()
-                            
+                    ForEach(navigationElements) { element in
+                        Spacer()
+                        NavigationLink(destination: element.destination) {
+                            VStack {
+                                Image(element.imageName)
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                Text(element.title).customTextStyle()
+                            }
                         }
-                    }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: Text("Element 2 Details")) {
-                        VStack {
-                            Image("ProgressSF") // Replace with your image
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                            Text("Progress").customTextStyle()
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: Text("Element 3 Details")) {
-                        VStack {
-                            Image("ProdcutsIconSF") // Replace with your image
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                            Text("Products").customTextStyle()
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: Text("Element 4 Details")) {
-                        VStack {
-                            Image("RoutineIconSF") // Replace with your image
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                            Text("Routine").customTextStyle()
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: Text("Element 5 Details")) {
-                        VStack {
-                            Image("CommunitySF") // Replace with your image
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                            Text("Community").customTextStyle()
-                        }
+                        Spacer()
                     }
                 }
                 .padding()
                 .frame(width: 350, height: 77)
                 .background(Color(#colorLiteral(red: 0.7803921699523926, green: 0.7058823704719543, blue: 1, alpha: 0.2))) // Background color
                 .cornerRadius(30)
-                
             }
         }
     }
-    }
-#Preview {
-    Navbar()
 }
+
 extension View {
     func customTextStyle() -> some View {
         self
@@ -89,4 +59,8 @@ extension View {
             .foregroundColor(.black)
             .frame(maxWidth: .infinity, minHeight: 12, maxHeight: 12, alignment: .center)
     }
+}
+
+#Preview {
+        Navbar()
 }

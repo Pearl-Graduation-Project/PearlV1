24192670ece2d0397d8c66179d37a6ae53e85e4d
ElAmir-Mansour <xxamirxx00@gmail.com>
2024-01-06 12:01:59 +0200

    modfied the tabbar to be customable and adding a custom item

---
+++ PearlV1/Components/TabBar/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 06/01/2024.
+//
+
+import Foundation
---
+++ PearlV1/Components/TabBar/TabBarItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarItemView.swift
+//  PearlV1
+//
+//  Created by ElAmir Mansour on 06/01/2024.
+//
+
+import SwiftUI
+
+struct TabBarItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarItemView()
+}
